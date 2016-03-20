d95e031a6b75a0668491850ea656ede305baf31d
Chloride Cull <chloride@devurandom.net>
2016-03-20 14:01:29 +0100

    clean up sandbox.py

--- sandbox.py
+++ sandbox.py
@@ -5,10 +5,11 @@ class csg2api:
     def __init__(self, bottleapp, sessionlist):
         self.app = bottleapp
         self.authhook = None
-        self.agpl = False # If True, allow deeper integration
         self.sessions = sessionlist
     def route(self, *args, **kwargs):
         return self.app.route(*args, **kwargs)
+    def get_request(self):
+        return request
     def get_username_of_request(self):
         return self.sessions[request.get_cookie("csg2sess")]
     def auth(self, func):
@@ -20,9 +21,7 @@ freshglobals = {
     "__loader__": globals()["__loader__"]
 }
 
-def create_box(sauce, bottleapp, apiclass=None, addglobals={}):
-    if apiclass == None:
-        apiclass = csg2api(bottleapp)
+def create_box(sauce, bottleapp, apiclass, addglobals={}):
     newglobals = copy.copy(freshglobals)
     newglobals["csg2api"] = apiclass
     newglobals.update(addglobals)
